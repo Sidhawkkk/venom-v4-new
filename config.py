205535544bb104a9bc90bc5b35d576f7f7d12d4d
Sidhawkkk <nawab2l2kf@gmail.com>
2024-09-06 00:02:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7462410233:AAFGmDSqnELGxdn9Dju_tP0GNo-SLzpVHLw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("hawksidd")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("602031659")
 
 
 
